e95446fa0a9a8285898eca7f8522919b85f17f62
S Anandaraj Srinivasan <SAnandaraj.Srinivasan@Honeywell.com>
2021-06-22 11:25:12 +0530

    Add project files.

---
+++ Send1/Program.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Send1
+{
+    class Program
+    {
+        static ITopicClient topicClient;
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+
+            string ServiceBusConnectionString = "Endpoint=sb://es-poc-service-bus.servicebus.windows.net/;SharedAccessKeyName=send-sub-key;SharedAccessKey=nTWKo5drapqfcGpjD0mwdyxI+GKXsdc2JJKE7ALsA5A=;";
+            string TopicName = "topic-alert";
+
+            topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+
+            // Send messages.
+            await SendUserMessage();
+
+            Console.WriteLine("Do you want send request from service 2? if yes 0 or No 1");
+
+            int hasRequest = Convert.ToInt32(Console.ReadLine());
+
+            if (hasRequest == 0)
+            {
+                await MainAsync();
+            }
+
+            Console.ReadKey();
+
+            await topicClient.CloseAsync();
+        }
+
+
+        static async Task SendUserMessage()
+        {
+            List<User> users = GetDummyDataForUser();
+
+            var serializeUser = JsonConvert.SerializeObject(users);
+
+            string messageType = "Service 2";
+
+            string messageId = Guid.NewGuid().ToString();
+
+            var message = new ServiceBusMessage
+            {
+                Id = messageId,
+                Type = messageType,
+                Content = serializeUser
+            };
+
+            var serializeBody = JsonConvert.SerializeObject(message);
+
+            // send data to bus
+
+            var busMessage = new Message(Encoding.UTF8.GetBytes(serializeBody));
+            busMessage.UserProperties.Add("Type", messageType);
+            busMessage.MessageId = messageId;
+
+            await topicClient.SendAsync(busMessage);
+
+            Console.WriteLine("message has been sent");
+
+        }
+
+        public class User
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        public class ServiceBusMessage
+        {
+            public string Id { get; set; }
+            public string Type { get; set; }
+            public string Content { get; set; }
+        }
+
+        private static List<User> GetDummyDataForUser()
+        {
+            User user = new User();
+            List<User> lstUsers = new List<User>();
+
+            for (int i = 1; i < 100; i++)
+            {
+                lstUsers.Add(new User { Id = i, Name = $"Felish {i}" });
+            }
+
+            return lstUsers;
+        }
+    }
+
+}
---
+++ azure-service-bus-sender/Program.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace azure_service_bus_sender
+{
+    class Program
+    {
+        static ITopicClient topicClient;
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+
+            string ServiceBusConnectionString = "Endpoint=sb://es-poc-service-bus.servicebus.windows.net/;SharedAccessKeyName=send-sub-key;SharedAccessKey=nTWKo5drapqfcGpjD0mwdyxI+GKXsdc2JJKE7ALsA5A=;";
+            string TopicName = "topic-alert";
+
+            topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+
+            Console.WriteLine("Enter the message count:");
+
+            int messageCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < messageCount; i++)
+            {
+                await SendUserMessage(i);
+            }
+            
+            // Send messages.
+
+            Console.WriteLine("Do you want send request from service 1? if yes 0 or press any for exist");
+            
+            int hasRequest = Convert.ToInt32(Console.ReadLine());
+
+            if (hasRequest == 0)
+            {
+                await MainAsync();
+            }
+
+            Console.ReadKey();
+
+            await topicClient.CloseAsync();
+        }
+
+        static async Task SendUserMessage(int serialNo)
+        {
+            List<User> users = GetDummyDataForUser();
+
+            var serializeUser = JsonConvert.SerializeObject(users);
+
+            string messageType = "Service 1";            
+
+            var message = new ServiceBusMessage
+            {
+                Id = serialNo,
+                Type = messageType,
+                Content = serializeUser
+            };
+
+            var serializeBody = JsonConvert.SerializeObject(message);
+
+            // send data to bus
+
+            var busMessage = new Message(Encoding.UTF8.GetBytes(serializeBody));
+            busMessage.UserProperties.Add("Type", messageType);
+            busMessage.MessageId = messageId;
+
+            await topicClient.SendAsync(busMessage);
+
+            Console.WriteLine("message has been sent");
+
+        }
+
+        public class User
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        public class ServiceBusMessage
+        {
+            public int Id { get; set; }
+            public string Type { get; set; }
+            public string Content { get; set; }
+        }
+
+        private static List<User> GetDummyDataForUser()
+        {
+            User user = new User();
+            List<User> lstUsers = new List<User>();
+            
+            for (int i = 1; i < 200; i++)
+            {                
+                lstUsers.Add(new User { Id = i , Name = $"Anandaraj {i}"}); 
+            }
+
+            return lstUsers;
+        }
+    }
+
+}
